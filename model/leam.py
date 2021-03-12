ddfb6d80d8dc2b374dcf90a1f98d5620d0158b34
chez8990 <chez8990@gmail.com>
2021-03-12 19:51:37 +0800

    replace loop with conv2d

--- model/leam.py
+++ model/leam.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 
@@ -7,28 +8,32 @@ class LEAM(nn.Module):
 
         if word_embedding is not None and label_embedding is not None:
             assert word_embedding.shape[1] == label_embedding.shape[1]
-            self.V = nn.Embedding(vocab_size, embed_dim, _weight=word_embedding)
-            self.C = label_embedding
+            assert torch.is_tensor(word_embedding) and torch.is_tensor(label_embedding)
+
+            self.V = nn.Embedding(vocab_size, embed_dim, _weight=word_embedding, padding_idx=0)
+            self.C = nn.Parameter(label_embedding)
         else:
             self.V = nn.Embedding(vocab_size, embed_dim)
-            self.C = nn.Embedding(n_classes, embed_dim)
+            self.C = nn.Parameter(torch.normal(0, 1, size=(n_classes, embed_dim)))
 
-        self.P = word_embedding.shape[1]
+        self.P = embed_dim
         self.K = n_classes
         self.R = phrase_window
 
-        self.f1_w = torch.nn.Parameter(torch.normal(0, 1, size=(self.R*2+1, )), requires_grad=True)
-        self.f1_b = torch.nn.Parameter(torch.normal(0, 1, size=(self.K, )))
+        # self.f1_w = torch.nn.Parameter(torch.normal(0, 1, size=(self.R*2+1, )), requires_grad=True)
+        # self.f1_b = torch.nn.Parameter(torch.normal(0, 1, size=(self.K, )))
+        self.f1 = nn.Conv2d(1, 1, (1, 2*self.R+1), bias=True)
         self.f2 = nn.Linear(self.P, self.K)
         self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(.5)
 
     def _embed_sentence(self, x):
         x = self.V(x)
         return x
 
     def _label_attention(self, x):
-        x_hat = torch.divide(x, torch.norm(x, dim=2, keepdim=True))
-        l_hat = torch.divide(self.C, torch.norm(self.C, dim=1, keepdim=True))
+        x_hat = torch.divide(x, torch.norm(x, dim=2, keepdim=True)+1e-3)
+        l_hat = torch.divide(self.C, torch.norm(self.C, dim=1, keepdim=True)+1e-3)
 
         G_hat = torch.matmul(l_hat.unsqueeze(dim=0), x_hat.permute(0, 2, 1))
         return G_hat
@@ -42,13 +47,21 @@ class LEAM(nn.Module):
 
         # there is a better way to implement label-phrase consistency
         # by reshaping G_hat, that is left for future work
-        m = []
-        for i in range(x.shape[1]):
-            G_sub = G_hat[..., i:i+self.R*2+1]
-            u_l = self.relu(torch.matmul(G_sub, self.f1_w) + self.f1_b)
-            m.append(u_l)
-        m = torch.stack(m, dim=-1)
+        # m = []
+        # for i in range(self.R, self.R + x.shape[1]):
+        #     G_sub = G_hat[..., i-self.R:i+self.R+1]
+        #     u_l = self.relu(torch.matmul(G_sub, self.f1_w) + self.f1_b)
+        #     m.append(u_l)
+        #
+        # m = torch.stack(m, dim=-1)
 
-        beta = torch.softmax(m.max(dim=1)[0], dim=1).reshape(-1, x.shape[1], 1)
+        m = self.f1(G_hat.unsqueeze(dim=1)).squeeze(dim=1)
+        m = self.relu(m)
+
+        m = m.max(dim=1)[0]
+
+        beta = torch.softmax(m, dim=1).reshape(-1, x.shape[1], 1)
         x = torch.mean(beta * x, dim=1)
-        return self.f2(x)
+        x = self.dropout(x)
+        x = self.f2(x)
+        return x
